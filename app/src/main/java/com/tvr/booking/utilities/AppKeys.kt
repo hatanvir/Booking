1128b8a948d9d1997af49809f709768f528329f0
hatanvirIntele <mhasan@intelehealth.org>
2024-11-16 14:49:49 +0600

    Appkeys added

---
+++ app/src/main/java/com/tvr/booking/utilities/AppKeys.kt
@@ -0,0 +1,14 @@
+package com.tvr.booking.utilities
+
+import java.sql.Struct
+
+/**
+ * Created By Tanvir Hasan on 11/15/24 9:36 AM
+ * Email: tanvirhasan553@gmail.com
+ */
+class AppKeys {
+    companion object {
+        const val PROPERTIES: String = "properties"
+        const val PROPERTY: String = "property"
+    }
+}
