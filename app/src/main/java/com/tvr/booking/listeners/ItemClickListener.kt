a5c47058af93299bfa73a86311af2ee9165e5132
hatanvirIntele <mhasan@intelehealth.org>
2024-11-16 14:56:52 +0600

    Item click listener added to handle click

---
+++ app/src/main/java/com/tvr/booking/listeners/ItemClickListener.kt
@@ -0,0 +1,9 @@
+package com.tvr.booking.listeners
+
+/**
+ * Created By Tanvir Hasan on 11/15/24 10:53 PM
+ * Email: tanvirhasan553@gmail.com
+ */
+interface ItemClickListener {
+    fun onClick(data: Any)
+}
