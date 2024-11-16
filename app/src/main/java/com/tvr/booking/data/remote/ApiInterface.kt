d278e48f83124caa810a82ad216f3e12a5eff8df
hatanvirIntele <mhasan@intelehealth.org>
2024-11-16 14:55:00 +0600

    Get properties api added

--- app/src/main/java/com/tvr/booking/data/remote/ApiInterface.kt
+++ app/src/main/java/com/tvr/booking/data/remote/ApiInterface.kt
@@ -1,8 +1,16 @@
 package com.tvr.booking.data.remote
 
+import com.tvr.booking.data.models.Property
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Url
+
 /**
  * Created By Tanvir Hasan on 11/13/24 9:56 PM
  * Email: tanvirhasan553@gmail.com
  */
 interface ApiInterface {
+    @GET("/")
+    suspend fun getProperty(): Response<List<Property>>
+
 }
