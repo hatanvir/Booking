bdcf945b06730bbcac89d9b3647c102dd7b94eec
hatanvirIntele <mhasan@intelehealth.org>
2024-11-16 14:56:33 +0600

    Home fragment added and all features implemented

--- app/src/main/java/com/tvr/booking/features/home/HomeFragment.kt
+++ app/src/main/java/com/tvr/booking/features/home/HomeFragment.kt
@@ -1,23 +1,73 @@
 package com.tvr.booking.features.home
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.tvr.booking.R
+import com.tvr.booking.databinding.FragmentHomeBinding
+import com.tvr.booking.features.allproperty.AllPropertyActivity
+import com.tvr.booking.features.propertydetails.PropertyDetailsActivity
+import com.tvr.booking.utilities.AppKeys
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+
+
 /**
  * Created By Tanvir Hasan on 11/13/24 10:22 PM
  * Email: tanvirhasan553@gmail.com
  */
 
+@AndroidEntryPoint
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var viewModel: HomeViewModel
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+
+        binding.seeAllTv.setOnClickListener {
+            startActivity(Intent(requireActivity(), AllPropertyActivity::class.java).apply {
+                putExtra(AppKeys.PROPERTIES, ArrayList(viewModel.propertyList))
+            })
+        }
+
+        binding.appCompatEditText.setOnClickListener {
+            binding.appCompatEditText.requestFocus()
+        }
+
+        handleSelectedProperty()
+        return binding.root
     }
+
+    private fun handleSelectedProperty() {
+        lifecycleScope.launch {
+            viewModel.selectedProperty.collect{
+                startActivity(Intent(requireActivity(), PropertyDetailsActivity::class.java).apply {
+                    putExtra(AppKeys.PROPERTY, it)
+                })
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.activity = requireActivity()
+        binding.lifecycleOwner = this.viewLifecycleOwner
+        binding.viewmodel = viewModel
+    }
+
 
 }
--- app/src/main/java/com/tvr/booking/features/home/HomeViewModel.kt
+++ app/src/main/java/com/tvr/booking/features/home/HomeViewModel.kt
@@ -1,10 +1,108 @@
 package com.tvr.booking.features.home
 
+import android.util.Log
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tvr.booking.BookingApplication
+import com.tvr.booking.R
+import com.tvr.booking.base.ViewState
+import com.tvr.booking.data.models.Category
+import com.tvr.booking.data.models.Property
+import com.tvr.booking.data.repository.PropertyRepo
+import com.tvr.booking.features.home.adapters.CategoryAdapter
+import com.tvr.booking.features.home.adapters.PropertyAdapter
+import com.tvr.booking.listeners.ItemClickListener
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 /**
  * Created By Tanvir Hasan on 11/13/24 9:56 PM
  * Email: tanvirhasan553@gmail.com
  */
-class HomeViewModel : ViewModel() {
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(private var propertyRepo: PropertyRepo) : ViewModel(),ItemClickListener {
+    var categoryAdapter = CategoryAdapter()
+    var propertyAdapter = PropertyAdapter()
+
+    var progressBarVisibility = MutableStateFlow(false)
+    var error = MutableStateFlow("")
+    var selectedProperty = MutableSharedFlow<Property>()
+
+    var propertyList: List<Property> = emptyList()
+
+    init {
+        getCategories()
+        getProperties()
+    }
+
+    /**
+     * populating property here
+     */
+    private fun getCategories() {
+        categoryAdapter.setData(
+            listOf(
+                Category(
+                    BookingApplication.instance.getString(R.string.flight),
+                    AppCompatResources.getDrawable(BookingApplication.instance, R.drawable.flight)
+                ),
+                Category(
+                    BookingApplication.instance.getString(R.string.hotels),
+                    AppCompatResources.getDrawable(BookingApplication.instance, R.drawable.hotel)
+                ),
+                Category(
+                    BookingApplication.instance.getString(R.string.visa),
+                    AppCompatResources.getDrawable(BookingApplication.instance, R.drawable.visa)
+                ),
+                Category(
+                    BookingApplication.instance.getString(R.string.buses),
+                    AppCompatResources.getDrawable(BookingApplication.instance, R.drawable.buses)
+                )
+            )
+        )
+        categoryAdapter.notifyDataSetChanged()
+    }
+
+    /**
+     * getting property data from remote
+     */
+    private fun getProperties() {
+        viewModelScope.launch {
+            propertyRepo.getProperty().collect {
+                when (it) {
+                    is ViewState.Loading -> {
+                        progressBarVisibility.value = true
+                    }
+
+                    is ViewState.Success -> {
+                        progressBarVisibility.emit(false)
+                        Log.d("SSS", "success ${it.data}")
+                        it.data?.let { it1 ->
+                            propertyList = it1
+                            propertyAdapter.setData(it1,this@HomeViewModel)
+                            propertyAdapter.notifyDataSetChanged()
+                        }
+                    }
+
+                    is ViewState.Error -> {
+                        progressBarVisibility.emit(false)
+                        error.emit(it.message ?: "Errorx")
+                        Log.d("Errr", it.message ?: "ttt")
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onClick(data: Any) {
+        viewModelScope.launch {
+            selectedProperty.emit(data as Property)
+        }
+    }
 }
